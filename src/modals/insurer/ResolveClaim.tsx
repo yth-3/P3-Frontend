8a70d6ab0f9f391eb99db55e9f7817d97bbdab30
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2023-01-10 15:50:10 -0600

    styled insurer dashboard, claims page

--- src/modals/insurer/ResolveClaim.tsx
+++ src/modals/insurer/ResolveClaim.tsx
@@ -15,30 +15,39 @@ export default function ResolveClaim() {
   }
 
   return (
-    <div>
-      <h3><b>Claim ID:</b> {claim?.id}</h3>
-      <h3><b>Submitter ID:</b> {claim?.submitterId}</h3>
-      <h3><b>Date submitted:</b> {claim?.submitted.toUTCString()}</h3>
-      <h3><b>Amount claimed:</b>  {claim?.claimed}</h3>
-      <h3><b>Claim type:</b>  {claim?.type}</h3>
-      <h3><b>Claim description:</b>  {claim?.description}</h3>
-      <h3><b>Claim status:</b>  {claim?.status}</h3>
-      { claim?.receipt &&
-        <h3><b>Claim receipt:</b>  {claim?.receipt}</h3>
-      }
-      {
-        claim?.resolverId ?
+    <>
+      { claim &&
+        <div>
           <>
-            <h3><b>Resolver ID:</b>  {claim?.resolverId}</h3>
-            <h3><b>Date resolved:</b>  {claim?.resolved?.toUTCString()}</h3>
-            <h3><b>Amount settled:</b>  {claim?.settled}</h3>
+            <h3><b>Claim ID:</b> {claim?.id}</h3>
+            <h3><b>Submitter ID:</b> {claim?.submitterId}</h3>
+            <h3><b>Date submitted:</b> {claim?.submitted.toUTCString()}</h3>
+            <h3><b>Amount claimed:</b> ${claim?.claimed}</h3>
+            <h3><b>Claim type:</b> {claim?.type}</h3>
+            <h3><b>Claim description:</b> {claim?.description}</h3>
+            <h3><b>Claim status:</b> {claim?.status}</h3>
           </>
-        :
-          <section className='flex flex-row justify-center gap-5'>
-            <ApproveButton onClick={() => approve()} />
-            <DenyButton onClick={() => deny()}/>
-          </section>
+          { claim?.receipt &&
+            <h3><b>Claim receipt:</b> {claim?.receipt}</h3>
+          }
+          {
+            claim?.resolverId ?
+              <>
+                <h3><b>Resolver ID:</b> {claim?.resolverId}</h3>
+                <h3><b>Date resolved:</b> {claim?.resolved?.toUTCString()}</h3>
+                <h3><b>Amount settled:</b> ${claim?.settled}</h3>
+              </>
+            :
+              <section className='flex flex-col'>
+                <h3 className='text-blue-600 font-bold text-center py-5'>Once this claim has been resolved, its status cannot be changed.</h3>
+                <div className='flex flex-row justify-center gap-5'>
+                  <ApproveButton onClick={() => approve()} />
+                  <DenyButton onClick={() => deny()}/>
+                </div>
+              </section>
+          }
+        </div>
       }
-    </div>
+    </>
   )
 }
--- src/pages/insurer/InsurerDashboardPage.tsx
+++ src/pages/insurer/InsurerDashboardPage.tsx
@@ -1,30 +1,32 @@
-import { useSetRecoilState } from 'recoil';
-import { useNavigate } from 'react-router-dom';
+import { principalState } from '../../App';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { modalState } from '../../App';
 import './InsurerDashboardPage.css';
+import LargeLink from '../../components/ui/LargeLink';
 import LargeButton from '../../components/ui/LargeButton';
 import { FIND_PATIENT_CLAIM } from '../../utility/constants';
 
 export default function InsurerDashboardPage() {
+    const principal = useRecoilValue(principalState);
     const setModal = useSetRecoilState(modalState);
-    const navigate = useNavigate();
 
     return (
         <main className='flex flex-col gap-10 items-center'>
-            <header id="insurer-header" className='text-lg flex flex-col items-center pt-8'>
+            <header
+                id="insurer-header"
+                className='flex flex-col items-center pt-8'
+            >
                 <div
                     id="header-copy"
                     className='bg-slate-50 opacity-90 w-fit p-4 rounded-lg text-2xl gap-6 flex flex-col text-blue-800'
                 >
-                    <h2 className='text-6xl'>Insurer Dashboard</h2>
-                    <div>Welcome!</div>
+                    <h2 className='text-4xl'>Welcome, {principal?.username}</h2>
                 </div>
             </header>
 
-            <section className='flex flex-row gap-5 items-center text-lg'>
-                Actions:
-                <LargeButton onClick={() => navigate("claims")}>Manage claims</LargeButton>
+            <section className='flex gap-4'>
+                <LargeLink to="claims">Manage claims</LargeLink>
                 <LargeButton onClick={() => setModal(FIND_PATIENT_CLAIM)}>Resolve claim</LargeButton>
             </section>
         </main>
