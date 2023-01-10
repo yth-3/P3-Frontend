dd3cece17c385f255ae14a09d4b35c2c3a6e4b7b
Michael-Robertson2 <34201714+Michael-Robertson2@users.noreply.github.com>
2023-01-10 15:17:59 -0800

    UserList Progress
    I'm starting to get the actual idea together, but I am unfortunately not there yet.

--- src/pages/admin/UserListPage.tsx
+++ src/pages/admin/UserListPage.tsx
@@ -3,22 +3,46 @@ import { useNavigate } from 'react-router-dom';
 
 import { backendApi } from '../../utility/api';
 import { principalState } from '../../App';
+import { User } from '../../utility/types';
 
 export default function UserListPage() {
     
     const navigate = useNavigate();
     const principal = useRecoilValue(principalState);
+    let users;
 
-    backendApi.get('/users', {
-        headers: {
-            authorization: principal?.token
-        }
-    })
-    .then((response) => console.log(response.data));
+    function getUsers() {
+        backendApi.get('/users', {
+            headers: {
+                authorization: principal?.token
+            }
+        }).then(response => {
+            //users = mapUsers(response.data);
+            console.log(response.data);
+        });
+    }
+
+    getUsers();
     return (
         <main>
             <h1>Users</h1>
+            <ul>
+                {users}
+            </ul>
         </main>
     )
+
+    function mapUsers(users: Array<User>) {
+        return users.map((user: User) => {    
+            return (
+                <li key={user.userId}>
+                    <div>{user.email}</div>
+                    <div>{user.username}</div>
+                    <div>{user.active}</div>
+                    <div>{user.registered}</div>
+                </li>
+            )
+        })
+    }
 }
 
