cfaba31f145aaa055e78351296a74eef624d1d2a
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2023-01-11 09:33:13 -0600

    removed all insurer modals from top level

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -2,5 +2,4 @@ export const LOGIN = 'login modal';
 export const SIGNUP = 'signup modal';
 export const LOGOUT = 'logout modal';
 export const VIEW_PATIENT_CLAIM = 'view patient claim modal';
-export const RESOLVE_PATIENT_CLAIM = 'resolve patient claim modal';
 export const PRINCIPAL = 'local storage key for principal';
