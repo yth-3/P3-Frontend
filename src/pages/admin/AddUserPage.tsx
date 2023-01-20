32da1da03db1fbe9f1a069d78c65ae56380ad3c9
Michael-Robertson2 <34201714+Michael-Robertson2@users.noreply.github.com>
2023-01-20 14:00:15 -0800

    Added the ability for admins to add Insurers and Staff members

---
+++ src/pages/admin/AddUserPage.tsx
@@ -0,0 +1,9 @@
+import AddUserForm from '../../components/forms/AddUserForm';
+
+export default function AddUserPage() {
+  return (
+    <>
+      <AddUserForm />
+    </>
+  );
+}
--- src/pages/admin/AdminDashboardPage.tsx
+++ src/pages/admin/AdminDashboardPage.tsx
@@ -5,24 +5,31 @@ import { useNavigate } from 'react-router-dom';
 import LargeButton from '../../components/ui/LargeButton';
 
 export default function AdminDashboardPage() {
-    // const setModal = useSetRecoilState(modalState);
-    const navigate = useNavigate();
+  // const setModal = useSetRecoilState(modalState);
+  const navigate = useNavigate();
 
-    return (
-        <main className='flex flex-col gap-10 items-center'>
-            <header id="landing-header" className='text-lg flex flex-col items-center pt-8'>
-                <div
-                    id="header-copy"
-                    className='bg-slate-50 opacity-90 w-fit p-4 rounded-lg text-2xl gap-6 flex flex-col text-blue-800'
-                >
-                    <h2 className='text-6xl'>Admin Dashboard</h2>
-                    <div>Welcome!</div>
-                    <section className='flex flex-row gap-5 justify-center text-lg'>
-                        <LargeButton onClick={() => navigate("/admin/users")}>View all users</LargeButton>
-                        <LargeButton onClick={() => navigate("/")}>Activate/inactivate user</LargeButton>
-                    </section>
-                </div>
-            </header>
-        </main>
-    )
+  return (
+    <main className='flex flex-col gap-10 items-center'>
+      <header
+        id='landing-header'
+        className='text-lg flex flex-col items-center pt-8'
+      >
+        <div
+          id='header-copy'
+          className='bg-slate-50 opacity-90 w-fit p-4 rounded-lg text-2xl gap-6 flex flex-col text-blue-800'
+        >
+          <h2 className='text-6xl'>Admin Dashboard</h2>
+          <div>Welcome!</div>
+          <section className='flex flex-row gap-5 justify-center text-lg'>
+            <LargeButton onClick={() => navigate('/admin/users')}>
+              View all users
+            </LargeButton>
+            <LargeButton onClick={() => navigate('/admin/add')}>
+              Add new Insurer/ Staff Member
+            </LargeButton>
+          </section>
+        </div>
+      </header>
+    </main>
+  );
 }
