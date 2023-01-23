cbb49e0e05a0d4c47ddd11d7a8b3fc4a501b3d80
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2023-01-23 13:44:27 -0600

    fixed UserInfo modal issues with insurer pages
    declaration fix
    
    added new version of UserInfo that does not contain Admin functionality

---
+++ src/modals/insurer/UserInfo.tsx
@@ -0,0 +1,47 @@
+import { useRecoilValue } from 'recoil';
+import { principalState } from '../../App';
+import { User } from '../../utility/types';
+
+type Props = {
+  user: User;
+};
+export default function UserInfo({ user }: Props) {
+  const principal = useRecoilValue(principalState);
+  return (
+    <>
+      {user && (
+        <div>
+          <h3>
+            <strong>User ID: </strong>
+            {user?.userId}
+          </h3>
+          <h3>
+            <strong>Username: </strong>
+            {user?.username}
+          </h3>
+          <h3>
+            <strong>Email address: </strong>
+            <a
+              href={'mailto:' + user?.email}
+              className='text-blue-600 hover:text-blue-500 hover:underline'
+            >
+              {user?.email}
+            </a>
+          </h3>
+          <h3>
+            <strong>Role: </strong>
+            {user?.role}
+          </h3>
+          <h3>
+            <strong>Date registered: </strong>
+            {user?.registered.substring(0, user?.registered.indexOf(' '))}
+          </h3>
+          <h3>
+            <strong>Activation status: </strong>
+            {user?.active ? 'Active' : 'Inactive'}
+          </h3>
+        </div>
+      )}
+    </>
+  );
+}
